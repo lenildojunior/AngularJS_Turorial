5e10e01672b0277cdd7ba30397265f1fcf0de5ae
lenildojunior <lenildo.ze@gmail.com>
2020-09-03 17:19:12 -0300

    Step 9 - Ruting and Multiple Views

--- app/app.module.js
+++ app/app.module.js
@@ -1,5 +1,8 @@
 // Define the `phonecatApp` module
 angular.module('phonecatApp', [
+	'ngRoute',
   // ...which depends on the `phoneList` module
-  'phoneList'
+	'phoneList',
+	'phoneDetail'
+
 ]);
